d987e8b6a1774a51e48e5d7d5cd3c6420ad024a9
aguiarjv <jvictoraguiar@live.com>
2023-06-05 21:29:05 -0300

    removed indent blankline nvim

--- lua/fion/packer.lua
+++ lua/fion/packer.lua
@@ -49,8 +49,6 @@ return require("packer").startup(function(use)
 
 	use("simrat39/symbols-outline.nvim")
 
-	use("lukas-reineke/indent-blankline.nvim")
-
 	-- Colorschemes
 	use("folke/tokyonight.nvim")
 	use("gruvbox-community/gruvbox")
