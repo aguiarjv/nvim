0ce05ea9df18e774325c1dc807229ae293278e6f
aguiarjv <jvictoraguiar@live.com>
2023-06-05 21:21:54 -0300

    small changes to lsp config file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,95 +7,96 @@ lspkind.init()
 
 local cmp = require("cmp")
 
-cmp.setup {
-  mapping = {
-    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-u>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<C-y>"] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { "i", "c" }
-    ),
+cmp.setup({
+	mapping = {
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-u>"] = cmp.mapping.scroll_docs(4),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-y>"] = cmp.mapping(
+			cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = true,
+			}),
+			{ "i", "c" }
+		),
 
-    ["<c-space>"] = cmp.mapping {
-      i = cmp.mapping.complete(),
-      c = function(
-        _ --[[fallback]]
-      )
-        if cmp.visible() then
-          if not cmp.confirm { select = true } then
-            return
-          end
-        else
-          cmp.complete()
-        end
-      end,
-    },
+		["<C-Space>"] = cmp.mapping({
+			i = cmp.mapping.complete(),
+			c = function(
+				_ --[[fallback]]
+			)
+				if cmp.visible() then
+					if not cmp.confirm({ select = true }) then
+						return
+					end
+				else
+					cmp.complete()
+				end
+			end,
+		}),
 
-    -- If you want tab completion :'(
-    --  First you have to just promise to read `:help ins-completion`.
-    --
-    -- ["<Tab>"] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    -- ["<S-Tab>"] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-  },
-  sources = {
-    { name = "gh_issues" },
-    { name = "cmp_tabnine" },
+		-- If you want tab completion :'(
+		--  First you have to just promise to read `:help ins-completion`.
+		--
+		-- ["<Tab>"] = function(fallback)
+		--   if cmp.visible() then
+		--     cmp.select_next_item()
+		--   else
+		--     fallback()
+		--   end
+		-- end,
+		-- ["<S-Tab>"] = function(fallback)
+		--   if cmp.visible() then
+		--     cmp.select_prev_item()
+		--   else
+		--     fallback()
+		--   end
+		-- end,
+	},
+	sources = {
+		{ name = "gh_issues" },
+		{ name = "cmp_tabnine" },
 
-    { name = "nvim_lua" },
+		{ name = "nvim_lua" },
 
-    { name = "nvim_lsp" },
-    { name = "path", keyword_length = 3},
-    { name = "luasnip" },
-    { name = "buffer", keyword_length = 5 },
-  },
+		{ name = "nvim_lsp" },
+		{ name = "path", keyword_length = 3 },
+		{ name = "luasnip" },
+		{ name = "buffer", keyword_length = 5 },
+	},
 
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 
-  formatting = {
-    format = lspkind.cmp_format {
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-        gh_issues = "[issues]",
-        cmp_tabnine = "[TabNine]",
-      },
-    },
-  },
+	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+				luasnip = "[snip]",
+				gh_issues = "[issues]",
+				cmp_tabnine = "[TabNine]",
+			},
+		}),
+	},
 
-  experimental = {
-    -- I like the new menu better! Nice work hrsh7th
-    native_menu = false,
+	experimental = {
+		-- I like the new menu better! Nice work hrsh7th
+		native_menu = false,
 
-    -- Let's play with this for a day or two
-    ghost_text = true,
-  },
-}
+		-- Let's play with this for a day or two
+		ghost_text = true,
+	},
+})
 
 -- Cmp Tabnine Plugin
 local tabnine = require("cmp_tabnine.config")
@@ -112,28 +113,52 @@ local function config(_config)
 	return vim.tbl_deep_extend("force", {
 		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = function()
-			nnoremap("gd", function() vim.lsp.buf.definition() end)
-			nnoremap("gD", function() vim.lsp.buf.declaration() end)
-			nnoremap("K", function() vim.lsp.buf.hover() end)
-			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-			nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
-			nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
-                filter = function(code_action)
-                    if not code_action or not code_action.data then
-                        return false
-                    end
+			nnoremap("gd", function()
+				vim.lsp.buf.definition()
+			end)
+			nnoremap("gD", function()
+				vim.lsp.buf.declaration()
+			end)
+			nnoremap("K", function()
+				vim.lsp.buf.hover()
+			end)
+			nnoremap("<leader>vws", function()
+				vim.lsp.buf.workspace_symbol()
+			end)
+			nnoremap("<leader>vd", function()
+				vim.diagnostic.open_float()
+			end)
+			nnoremap("[d", function()
+				vim.diagnostic.goto_next()
+			end)
+			nnoremap("]d", function()
+				vim.diagnostic.goto_prev()
+			end)
+			nnoremap("<leader>vca", function()
+				vim.lsp.buf.code_action()
+			end)
+			nnoremap("<leader>vco", function()
+				vim.lsp.buf.code_action({
+					filter = function(code_action)
+						if not code_action or not code_action.data then
+							return false
+						end
 
-                    local data = code_action.data.id
-                    return string.sub(data, #data - 1, #data) == ":0"
-                end,
-                apply = true
-            }) end)
-			nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
-			nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
-			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+						local data = code_action.data.id
+						return string.sub(data, #data - 1, #data) == ":0"
+					end,
+					apply = true,
+				})
+			end)
+			nnoremap("<leader>vrr", function()
+				vim.lsp.buf.references()
+			end)
+			nnoremap("<leader>vrn", function()
+				vim.lsp.buf.rename()
+			end)
+			inoremap("<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end)
 		end,
 	}, _config or {})
 end
