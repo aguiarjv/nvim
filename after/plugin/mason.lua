c1ec987ae81de810d8dffb624d0593c34921e516
aguiarjv <jvictoraguiar@live.com>
2024-01-04 11:07:56 -0300

    added default installations to mason

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,9 +1,9 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "pyright", "lua_ls", "emmet_ls", "cssls", "html", "tsserver", "gopls" },
+	ensure_installed = { "pyright", "pylsp", "lua_ls", "emmet_ls", "cssls", "tailwindcss", "html", "tsserver", "gopls" },
 })
 
 -- Needs to install python3-venv to install python formatter and debugger
 require("mason-tool-installer").setup({
-	ensure_installed = { "black", "stylua", "debugpy", "prettier" },
+	ensure_installed = { "black", "stylua", "debugpy", "prettier", "flake8", "eslint_d" },
 })
