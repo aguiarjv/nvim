5b7ee48e802506b38512d9398833f31f6f5a2f2e
aguiarjv <jvictoraguiar@live.com>
2025-04-24 09:11:13 -0300

    disabled snacks (indent, scroll, dim and other) animations

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            indent = {
+                enabled = true,
+                animate = {
+                    enabled = false,
+                },
+            },
+            scroll = {
+                enabled = false,
+            },
+            animte = {
+                enabled = false,
+            },
+        },
+    },
+}
