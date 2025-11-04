4f5ab4bb9794af96583e7cc177deb9e6056786fa
aguiarjv <jvictoraguiar@live.com>
2025-11-04 00:03:33 -0300

    wip

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,42 @@
 return {
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'java',
-        'typescript',
-        'javascript',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  version = false,
+  build = ':TSUpdate',
+  lazy = false,
+  config = function()
+    local TS = require 'nvim-treesitter'
+    TS.install {
+      'c',
+      'javascript',
+      'python',
+      'tsx',
+      'typescript',
+      --
+      'diff',
+      'html',
+      'bash',
+      'jsdoc',
+      'json',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'printf',
+      'query',
+      'regex',
+      'toml',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
+    }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
+    })
+  end,
 }
