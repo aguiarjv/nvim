7b5c56ff3f286e8b3aa559763844cca31e842e8e
aguiarjv <jvictoraguiar@live.com>
2025-11-04 00:14:03 -0300

    wip

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,39 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  branch = 'main',
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   version = false,
-  build = ':TSUpdate',
+  build = ":TSUpdate",
   lazy = false,
   config = function()
-    local TS = require 'nvim-treesitter'
+    local TS = require "nvim-treesitter"
     TS.install {
-      'c',
-      'javascript',
-      'python',
-      'tsx',
-      'typescript',
+      "c",
+      "javascript",
+      "python",
+      "tsx",
+      "typescript",
       --
-      'diff',
-      'html',
-      'bash',
-      'jsdoc',
-      'json',
-      'lua',
-      'luadoc',
-      'luap',
-      'markdown',
-      'markdown_inline',
-      'printf',
-      'query',
-      'regex',
-      'toml',
-      'vim',
-      'vimdoc',
-      'xml',
-      'yaml',
+      "diff",
+      "html",
+      "bash",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "query",
+      "regex",
+      "toml",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
     }
 
-    vim.api.nvim_create_autocmd('FileType', {
+    vim.api.nvim_create_autocmd("FileType", {
       callback = function(args)
         pcall(vim.treesitter.start, args.buf)
       end,
