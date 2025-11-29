43cc5c2b973ce2ea7032514ac6949ba45d48696e
aguiarjv <jvictoraguiar@live.com>
2025-11-29 14:43:35 -0300

    minor changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -134,6 +134,7 @@ return {
         end
 
         vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(args)
                 local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
 
