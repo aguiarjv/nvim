bbb1a790b27137a9696640748470705fcb95c731
aguiarjv <jvictoraguiar@live.com>
2025-11-29 08:39:40 -0300

    added suport for nvim api on lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,20 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
+        {
+            -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "luvit-meta/library", words = { "vim%.uv" } },
+                    { path = "/usr/share/awesome/lib/", words = { "awesome" } },
+                },
+            },
+        },
+        { "Bilal2453/luvit-meta", lazy = true },
         "mason.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         { "mason-org/mason-lspconfig.nvim", config = function() end },
