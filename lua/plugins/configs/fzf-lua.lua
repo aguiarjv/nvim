bafd45b23f5495ff84ce2704839711546076568d
Joao Aguiar <jvictoraguiar@live.com>
2026-02-24 01:49:32 -0300

    added fzf git commands

--- lua/plugins/configs/fzf-lua.lua
+++ lua/plugins/configs/fzf-lua.lua
@@ -37,9 +37,12 @@ return {
         { "<leader>fg", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
         { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
         -- git
-        { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
+        { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits (Project)" },
+        { "<leader>gC", "<cmd>FzfLua git_bcommits<CR>", desc = "Commits (Buffer)" },
+        { "<leader>gb", "<cmd>FzfLua git_blame<CR>", desc = "Blame" },
+        { "<leader>gB", "<cmd>FzfLua git_branches<CR>", desc = "Branches" },
+        { "<leader>gw", "<cmd>FzfLua git_worktrees<CR>", desc = "Worktrees" },
         { "<leader>gd", "<cmd>FzfLua git_diff<cr>", desc = "Git Diff (hunks)" },
-        { "<leader>gl", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
         { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
         { "<leader>gS", "<cmd>FzfLua git_stash<cr>", desc = "Git Stash" },
         -- search
