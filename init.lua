99a6ea8ed9519b62b2da5de86d6dc020bb3718b0
Joao Aguiar <joao.aguiar@intelie.com>
2026-03-03 00:15:37 -0300

    setting nvim default cache and data directories

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
+-- Changing default cache and data directories
+local user = vim.loop.os_get_passwd().username
+
+vim.env.XDG_CACHE_HOME = "/var/tmp/" .. user .. "/xdg-cache"
+vim.env.XDG_DATA_HOME = "/var/tmp/" .. user .. "/xdg-data"
+
+vim.fn.mkdir(vim.env.XDG_CACHE_HOME, "p")
+vim.fn.mkdir(vim.env.XDG_DATA_HOME, "p")
+
+-- Initialize neovim configs
 require("config.lazy")
