b6a736ae492d8dac8859f0d4157f1f963193ad03
Joao Aguiar <jvictoraguiar@live.com>
2026-04-19 15:56:56 -0300

    minor addition to conform

--- lua/plugins/configs/conform.lua
+++ lua/plugins/configs/conform.lua
@@ -7,7 +7,7 @@ return {
         {
             "<leader>cF",
             function()
-                require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+                require("conform").format({ async = true, formatters = { "injected" }, timeout_ms = 3000 })
             end,
             mode = { "n", "x" },
             desc = "Format Injected Langs",
